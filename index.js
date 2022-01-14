a56af5118b263ba173f9db9b66a5ca85111fc8d9
Jeremy Yip <jeremy.yip@automattic.com>
2022-01-14 12:12:05 -0800

    Initialize repo

---
+++ index.js
@@ -0,0 +1,11 @@
+const PORT = 6789;
+const axios = require("axios");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Test");
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
